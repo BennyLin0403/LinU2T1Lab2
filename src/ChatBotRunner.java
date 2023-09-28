46419eb1c0df60a33ee9b2901bf381aca58b1dc8
Benny Lin <bennylin050@gmail.com>
2023-09-28 12:35:49 -0400

    new

---
+++ src/ChatBotRunner.java
@@ -0,0 +1,19 @@
+public class ChatBotRunner {
+    public static void main(String[] args){
+        ChatBot debbie = new ChatBot("bob", 21);
+
+        debbie.greeting("Benny Lin");
+        debbie.weather();
+
+        debbie.favoriteNumber(7);
+
+
+
+        double feetToMetter = debbie.convertFeetToMeters(10);
+        System.out.println("There are " + feetToMetter + " in 10 feet!");
+        int sum = debbie.addNumbers(1,2,3);
+        System.out.println("The sum is " + sum);
+        String bye =    debbie.goodbye();
+        System.out.println(bye);
+    }
+}
